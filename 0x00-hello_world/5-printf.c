bb4e0af9d07a6ef43e501c1651c96d13713ab463
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-07 14:01:18 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - print a text with printf
+*
+* Return: 0 if successful.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
