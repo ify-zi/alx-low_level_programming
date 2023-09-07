d9c95afd722a219a7a4e892151de2043b1330a62
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-07 13:50:29 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints text "Programming is like building a multilingual puzzle".
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
