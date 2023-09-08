fd26ebb7e5d7d7206a6db5b316319ff0d9df89cf
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-08 15:38:21 -0700

    prints lowercase a..z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - starting point
+*
+* Return: return 0 when successful
+*/
+int main(void)
+{
+	char n;
+	
+	n = 'a';
+	while (n <= 'z')
+	{
+		putchar(n);
+		putchar('\n');
+		n++;
+	}
+	return (0);
+}
