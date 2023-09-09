1fe1358dc492d85bdfd45c9fff031246ccd64005
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-08 18:45:38 -0700

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - start point
+*
+* Return: this returens zero(0)
+*/
+
+int main(void)
+{
+	char n;
+
+	n = 'z';
+	while (n <= 'a')
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
