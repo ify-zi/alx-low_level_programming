ddb49606afee86a859cce7576882db192f1e7112
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-12 12:01:55 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: returns 0 if successful
+*/
+
+int main(void)
+{
+	const char *a = "_putchar";
+
+	int num;
+	for (num = 0; num != '\0'; num++)
+	{
+		putchar(a[num]);
+		putchar('\n');
+	}
+	return (0);
+}
