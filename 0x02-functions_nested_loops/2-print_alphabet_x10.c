2b0a1f226d6c0a8dd9f99db52979576cb0ccdedf
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-12 15:35:13 -0700

    print alpa 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints alphabet in lowercases x10
+*
+* Return: alway 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char let;
+	int i;
+
+	i = 0;
+	while (i < 9)
+	{
+		let = 'a';
+		while (let <= 'z')
+		{
+		_putchar(let);
+		let++;
+		}
+	_putchar('\n');
+	i++;
+	}
+}
