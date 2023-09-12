5e1729311138f4d884bde233a8093f501fd59507
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-12 16:26:42 -0700

    check type of int

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_sign - checks for the type of int
+* @n: take the value to be checked
+*
+* Return: 1 is positive, 0 if zero and -1 else
+*/
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
