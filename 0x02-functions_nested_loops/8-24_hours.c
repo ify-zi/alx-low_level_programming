42a60a388951ed9fd7f41a110eb3fdde6ecb0b69
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-13 01:56:49 -0700

    print the hours of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* jack_bauer - print the hours and mins in a day
+*/
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+	i++;
+	}
+}
