114efa4428557f0663f9d314fe51c0d9d2814ee7
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-13 04:59:37 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add - sums two numbers
+* @a: first number
+* @b: second number
+*
+* Return: return the value of the summation
+*/
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
