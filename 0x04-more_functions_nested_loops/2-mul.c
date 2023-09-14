7e18f429bd56d8f89903a8217e9a44cae3b3e356
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-14 14:43:37 -0700

    multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - multiple two integers
+* @a: first operand
+* @b: second operand
+*
+* Return: resul is success
+*/
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 extern int _putchar(char c);
 extern int _isupper(char c);
 extern int _isdigit(int c);
+extern int mul(int a, int b);
 
 #endif
