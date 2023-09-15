0cc654cb3ff56d27edb4c35e3b976f03cb688fac
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-14 17:11:53 -0700

    print square size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_square - print square of a size
+* @size: size of the square
+*/
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
