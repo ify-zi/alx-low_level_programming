0a28bfe3231c455cb5a7eec379a30fae966fd9b8
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-15 03:11:49 -0700

    largest figure

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,5 +24,17 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+	else if (a == b && b == c)
+	{
+		largest = a;
+	}
+	else if (a == b && b > c)
+	{
+		largest = a;
+	}
+	else if (a < b && b == c)
+	{
+		largest = b;
+	}
 	return (largest);
 }
