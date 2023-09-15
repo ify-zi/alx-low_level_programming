51e4b1508c7d12c99f9736be4819d3d342879ba4
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-14 18:03:56 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,15 @@ int main(void)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("%s", "FizzBuzz");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("%s", "Fizz");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("%s", "Buzz");
 		}
 		else
 		{
