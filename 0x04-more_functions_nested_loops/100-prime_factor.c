6e5eb0dd86b745b26c82b7004880681fc692e000
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-14 18:35:48 -0700

    mine

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,47 +1,24 @@
 #include <stdio.h>
-#include <inttypes.h>
 
 /**
-* largestPrimeFactor - find the largest factor
-* @n: accepts arg
-*
-*Return: maxprime
-*/
+ * main -  calculate largest prime of 612852475143
+ *
+ * Return: Success Always
+ */
 
-int64_t largestPrimeFactor(int64_t n)
-{
-	int64_t i;
-	int64_t maxPrime = -1;
-	
-	while (n % 2 == 0)
-	{
-		maxPrime = 2;
-		n /= 2;
-	}
 
-	for (i = 3; i * i <= n; i += 2)
-	{
-		while (n % i == 0)
-		{
-			maxPrime = i;
-			n /= i;
-		}
-	}
-	if (n > 2)
-	{
-		maxPrime = n;
-	}
-	return maxPrime;
-}
-/**
-* main - void of argusment
-*
-*Return: 0;
-*/
 int main(void)
 {
-	int64_t number = 612852475143;
-	int64_t result = largestPrimeFactor(number);
-	printf("%" PRId64 "\n", result);
-	return 0;
+	long int i = 612852475143;
+	long int a;
+
+	for (a = 2; a < x; a++)
+	{
+		if (x % a == 0)
+		{
+			x = x / a;
+		}
+	}
+	printf("%ld\n", a);
+	return (0);
 }
