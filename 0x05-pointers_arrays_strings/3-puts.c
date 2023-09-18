b4e160c8c58eb68fc7219fe7f7d9f919b9bb8613
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-18 07:20:52 -0700

    outputs a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - returns a str
+* @str: takes the memory location
+*/
+
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ extern int _putchar(char c);
 extern void reset_to_98(int *n);
 extern void swap_int(int *a, int *b);
 extern int _strlen(char *s);
+extern void _puts(char *str);
 
 #endif
