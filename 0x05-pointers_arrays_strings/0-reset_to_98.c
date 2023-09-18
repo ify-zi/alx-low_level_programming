ae2fdc4fdcbd7b72724a23d7c8e6a32888381f64
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-18 05:21:03 -0700

    my first pointer trial

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* reset_to_98 - reset any number to 98
+* int n: takes the argument
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN
+#define MAIN
+
+extern int _putchar(char c);
+extern void reset_to_98(int *n);
+
+#endif
