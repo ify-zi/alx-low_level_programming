15c9a98393f719b1382928f3e8fad7c2eb548bcf
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-18 07:52:29 -0700

    reverse a text

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* print_rev - returns the revese of a string
+* @s: first operand
+*/
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int a;
+
+	for (a = len; a <= len; a--)
+	{
+		if (a >= 0)
+		{
+			_putchar(s[a]);
+		}
+		else
+			break;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ extern void reset_to_98(int *n);
 extern void swap_int(int *a, int *b);
 extern int _strlen(char *s);
 extern void _puts(char *str);
+extern void print_rev(char *s);
 
 #endif
