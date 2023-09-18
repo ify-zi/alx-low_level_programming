f3480fbb6ca484d579128b283ff5b51f2b92e428
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-18 06:43:05 -0700

    retrun length of a string using pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen - returns the lenght of the string
+* @s: the collector of the arg
+*
+* Return: return the length of the string
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
