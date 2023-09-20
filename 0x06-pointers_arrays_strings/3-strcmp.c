5baacc21d4794f89ea094af9f04801a7e76b6c75
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-20 14:50:14 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp - comapres two strings
+* @s1: forst string
+* @s2: sex9nd string
+* Return: retrun an int
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
