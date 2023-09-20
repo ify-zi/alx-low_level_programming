c8b935e9e109e02a4bfaf436b7674f2adddebc12
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-20 14:28:38 -0700

    copying usijg strcat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* *_strncpy - copies a strong
+* @dest: destinat9on
+* @src: source
+* @n: colecr sam integer
+*
+* Return: return an integer
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 extern int _putchar(char c);
 extern char *_strcat(char *dest, char *src);
 extern char *_strncat(char *dest, char *src, int n);
+extern char *_strncpy(char *dest, char *src, int n);
 
 #endif
