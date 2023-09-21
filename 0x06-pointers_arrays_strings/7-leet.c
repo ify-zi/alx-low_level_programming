33ee9741fa5a50687addc8b45ddc2db161c52a87
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-21 13:35:27 -0700

    1337 encodes

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,33 @@
 #include "main.h"
-
 /**
-* *leet - encodes to 1337
-* @s: collects string)
-* Return: returns the char
-*/
-
+ * leet -  encodes a string into 1337.
+ * @s: string to code
+ * Return: the enconde string.
+ */
 char *leet(char *s)
 {
 	int i;
+	int j;
+	int k;
+
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '3';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '5';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	i++;
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
+		{
+			if (s[i] == l[j])
+			{
+				k = j;
+				s[i] = e[k];
+			}
+			j++;
+		}
+		i++;
 	}
 	return (s);
 }
