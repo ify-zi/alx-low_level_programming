ebf3c80184f308b9de73eab7fcc069711d1d0a5d
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-25 06:04:57 -0700

    searcher

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* *_strchr - searches fir a character
+* @s: string to searchbin
+* @c: the character to find
+*
+* Return: return a value
+*/
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
