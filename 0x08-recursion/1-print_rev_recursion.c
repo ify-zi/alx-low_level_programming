db0a71a4c9be96d87f933f6e48bdea432583e8f6
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-09-28 11:07:43 -0700

    print reverse using recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _print_rev_recursion - reverse of a string
+* @s: string collector
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
