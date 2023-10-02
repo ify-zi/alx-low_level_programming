6a84c22206629678b5bd17ff112ed65da1e5638f
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-10-02 07:57:50 -0700

    returns the count of the args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints the number of args
+* @argc: args count
+* @argv: the arggs strings
+* Return: returns 0 if successful
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
