c763249998ccad5c2cce5433929dc7fbcae4dfba
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-10-02 12:11:57 -0700

    multply the args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print the multiple of the number
+* @argc: the count
+* @argv: the array of string
+* Return: returns 0 if successful
+*/
+
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
