09db32ed793eb5778172945232172049421012f9
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-10-02 12:26:03 -0700

    multplying args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,18 +13,18 @@ int main(int argc, char *argv[])
 	int mul = 1;
 	int i;
 
-	if (argc > 1)
+	if (argc != 3)
 	{
-		for (i = 1; i < argc; i++)
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < 3; i++)
 		{
 			mul = mul * atoi(argv[i]);
 		}
 		printf("%d\n", mul);
 		return (0);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
 	}
 }
