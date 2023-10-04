6e0a3ab203fa66ebbab6ff774b839377ef344cb1
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-10-03 17:35:13 -0700

    printing a two dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** **alloc_grid - allocates all the matrix
+/**
+* **alloc_grid - allocates all the matrix
 * @width: width of the array
 * @height: the height of the array
 * Return: return the string on the array
@@ -19,7 +20,8 @@ int **alloc_grid(int width, int height)
 	if (height <= 0)
 		return (NULL);
 
-	img = malloc(sizeof(int *) * height); 
+	img = malloc(sizeof(int *) * height);
+
 	if (img == NULL)
 	{
 		free(img);
