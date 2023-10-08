da815e99d5e96eac8ef5d2003f6e4af03c8f2977
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-10-07 17:17:33 -0700

    concat the strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,12 +33,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= len1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-	for (j = 0; j <= n; j++)
-	{
+	for (j = 0; j < n; j++)
 		ptr[i + j] = s2[j];
-	}
 	ptr[i + j] = '\0';
+
 	return (ptr);
 }
