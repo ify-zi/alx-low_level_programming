3a247b8fbaf4a4949e6fe2d8208a04a50e65509a
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-10-07 17:50:40 -0700

    correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-	for (j = 0; j < nm; j++)
+	for (j = 0; j <= nm; j++)
 		ptr[i + j] = s2[j];
 	ptr[i + j] = '\0';
 
