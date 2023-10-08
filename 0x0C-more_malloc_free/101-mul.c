1d2c5f9f4b5c5c23633a335eb1e33ea7146e5fc5
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-10-08 12:35:01 -0700

    print the multiples of two args

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int i, j;
+	char msg[] = "Error";
+
+	if (argc != 3)
+	{
+		for (i = 0; i < 5; i++)
+			_putchar(msg[i]);
+		_putchar('\n');
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				for (i = 0; i < 5; i++)
+					_putchar(msg[i]);
+				_putchar('\n');
+				exit(98); 
+			}
+		}
+	}
+	mul = atoi(argv[1]) *  atoi(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,7 @@ extern char *string_nconcat(char *s1, char *s2, unsigned int n);
 extern void *_calloc(unsigned int nmemb, unsigned int size);
 extern int *array_range(int min, int max);
 extern void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+extern int main(int argc, char *argv[]);
+extern int _putchar(char c);
 
 #endif
