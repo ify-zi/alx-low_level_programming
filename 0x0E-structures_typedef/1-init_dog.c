7d7ed152839eb66e9aadb895271419ded9c388bc
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-10-10 15:17:31 -0700

    struct and typdef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+* init_dog - initalises a struct
+* @d: pointer to the name of the instance
+* @name: dog name
+* @age: age of dog
+* @owner: the owner of the dog
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
