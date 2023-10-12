642ee3e35be6602cf2a5533fbb9ee2906cd854f7
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2023-10-12 14:17:42 -0700

    understanding variadic func

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,18 +5,18 @@
 * sum_them_all - sum numbers of certain number of args
 * @n: const count that cant be changed
 *
-* Return: the sum to all the code
-* Description: code take a variable number of args and returns the sum
+* Return: the sum or 0 if fail
 */
 
 int sum_them_all(const int n, ...)
 {
 	int i, sum, num;
 	va_list args;
+	
 	va_start(args, n);
 
 	if (n == 0)
-		return(0);
+		return (0);
 
 	sum = 0;
 	for (i = 0; i < n; i++)
