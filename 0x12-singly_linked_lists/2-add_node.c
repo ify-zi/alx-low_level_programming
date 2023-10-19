37aa7aad5a5c9cd5ed2fb2b9fcd6735ee475de0a
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-10-19 15:12:53 -0700

    singly linkd list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* add_node - add aa nod to sinlge list
+* @head: takes the head of he code
+* @str: name of character
+*
+* Return: the length of each
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int len = 0;
+	list_t *new;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
