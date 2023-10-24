efd93c9cb86ff22d9459414a2a09387a59057263
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-10-23 19:55:53 -0700

    free head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees mem block
+ * @head: head of node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		curr = (*head)->next;
+		free(*head);
+		*head = curr;
+	}
+}
