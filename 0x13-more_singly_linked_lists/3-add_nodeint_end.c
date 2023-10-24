12b5413ac33889e356c4631f356b55ca200aec95
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-10-23 19:19:21 -0700

    add at end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint_end - adds a node at the ned of the list
+ * @head: the head of the node
+ * @n: intger
+ * Return: retrun mem addr or NULL
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *curr;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	curr = *head;
+	while (curr->next)
+		curr = curr->next;
+
+	curr->next = new_node;
+
+	return (new_node);
+}
