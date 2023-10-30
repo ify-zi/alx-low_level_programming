b518d43c6288d7099dd0f4c5b9819a22bd7886c6
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-10-30 15:11:43 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,12 +22,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		while (text_content[num])
-		{
 			num++;
-			i = write(fp, text_content, num);
-			if (i != num)
-				return (-1);
-		}
+		i = write(fp, text_content, num);
+		if (i != num)
+			return (-1);
 	}
 
 	close(fp);
