72dd6153ac91de9e57f38ec9564f9b3d14ffcd6a
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-12-09 07:55:09 -0800

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: pointer to list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	current = head;
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+
+	while (current->prev != NULL)
+	{
+		free(current);
+		current = current->prev;
+	}
+
+	free(current);
+}
