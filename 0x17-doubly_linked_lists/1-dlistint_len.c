d57a71beea06ce03d4aecd94d95840666cfcafb7
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-12-09 04:38:53 -0800

    retunr len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * dlisint_len - returns lenght of the linked list
+ * @h: pointer to the start of list
+ * Return: return the number of list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+	const dlistint_t *cpy;
+
+	cpy = h;
+
+	while (cpy != NULL)
+	{
+		cpy = cpy->next;
+		len++;
+	}
+
+	return (len);
+}
