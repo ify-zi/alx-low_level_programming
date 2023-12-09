12a45894d1824f9aa3c29c4cd2346860053f0283
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-12-09 05:28:21 -0800

    add at beginning

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node at beginning
+ * @h: pointer to head
+ * @n: int at node
+ * Return: return address of node or null if failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *current, *new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		*head = new_node;
+		return (*head);
+	}
+	current = *head;
+	new_node->next = current;
+	current->prev = current;
+	*head = new_node;
+
+	return (new_node);
+}
