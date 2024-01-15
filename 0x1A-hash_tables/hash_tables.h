75f350a9c94dbe52278c1a6f1ffe3f2c9d979b72
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-01-15 12:14:20 -0800

    betty correction

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
 #ifndef HASH_H
-#define HASH_H 
+#define HASH_H
 
 /* Librray declaration */
 #include <stdio.h>
@@ -15,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,8 +30,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 /* functions */
